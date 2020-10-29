0af1391f4e78f512098981de5c2a7b5cf8c52937
gbazi <germain.bazin@smatis.fr>
2020-10-29 09:44:44 +0100

    init

---
+++ src/main/java/fr/germain/hfrbattle/HfrBattleApplication.java
@@ -0,0 +1,13 @@
+package fr.germain.hfrbattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HfrBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HfrBattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/germain/hfrbattle/HfrBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.germain.hfrbattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HfrBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
